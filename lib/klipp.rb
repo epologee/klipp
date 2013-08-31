70700d4a8a367cbfe7fa70edadc596e2ce5dad50
Jankees van Woezik <jankees@base42.nl>
2013-08-31 15:48:52 +0200

    Updated the spec_helper, added the requires to klipp.rb

--- lib/klipp.rb
+++ lib/klipp.rb
@@ -1,7 +1,4 @@
 require "klipp/version"
-
-module Klipp
-
-  require 'klipp/command'
-
-end
+require "klipp/template"
+require "klipp/token"
+require 'klipp/command'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,26 @@
-require 'coveralls'
-Coveralls.wear!
+if ENV['CI'] || ENV['GENERATE_COVERAGE']
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.start
 
-if Dir.glob('lib/**/*.rb').map { |f| require "./#{f}" }.include? false
-  raise 'error loading files'
+  if ENV['CI']
+    SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  elsif ENV['GENERATE_COVERAGE']
+    SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+  end
+
 end
+
+#Dir.glob('lib/**/*.rb').each do |file|
+#  required = require file
+#  p "required: #{required} file: #{file}"
+#end
+
+require 'klipp'
+
+#if Dir.glob('lib/**/*.rb').map { |f| p f; require "./#{f}"}.include? false
+#  raise 'error loading files'
+#end
 
 def read_fixture fixture_name
   File.read(File.join(__dir__, 'fixtures', fixture_name))
